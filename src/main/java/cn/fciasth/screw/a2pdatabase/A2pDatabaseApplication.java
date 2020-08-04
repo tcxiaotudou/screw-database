5fb6df53b7b8160b41b0bb9e0194ef371992968c
tcxiaotudou <2290262044@qq.com>
2020-08-04 22:51:09 +0800

    screw-database init

---
+++ src/main/java/cn/fciasth/screw/a2pdatabase/A2pDatabaseApplication.java
@@ -0,0 +1,13 @@
+package cn.fciasth.screw.a2pdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class A2pDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(A2pDatabaseApplication.class, args);
+    }
+
+}
